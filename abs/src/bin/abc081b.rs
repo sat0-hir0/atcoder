b8702f60a92ee8c9c435e88972cb5252a97981bb
sat0-hir0 <sat0-hir0@users.noreply.github.com>
2021-01-31 17:18:19 +0900

    add: abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,16 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i32; n],
+    }
+    print!(
+        "{}",
+        a.into_iter()
+            .map(|num: i32| num.trailing_zeros())
+            .min()
+            .unwrap()
+    );
 }
