a85128cd019873b95a42113581ed119e97b00686
sat0-hir0 <sat0-hir0@users.noreply.github.com>
2021-01-31 14:50:41 +0900

    add: abc081a

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -1,3 +1,18 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        s: i32
+    }
+
+    let mut count = 0;
+
+    let input_s: i32 = s;
+    for num in input_s.to_string().chars() {
+        if num == '1' {
+            count += 1;
+        }
+    }
+    print!("{}", count);
 }
