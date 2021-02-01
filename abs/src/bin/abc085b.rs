129b1014dfa4532f049c18296a894711a7ecbb05
sat0-hir0 <sat0-hir0@users.noreply.github.com>
2021-02-02 00:11:57 +0900

    comment: abc085bを追加

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,3 +1,12 @@
+use proconio::{fastout, input};
+use std::collections::BTreeSet;
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: i8,
+        d: [i8; n]
+    }
+    let result: BTreeSet<i8> = d.into_iter().map(|num| num as i8).collect();
+    print!("{}", result.len());
 }
