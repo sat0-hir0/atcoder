e915a4ea6d5d570803a419c2e8a3e7bc0d513df9
sat0-hir0 <sat0-hir0@users.noreply.github.com>
2021-02-01 21:56:34 +0900

    comment: abc083bを追加

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,22 @@
+use proconio::{fastout, input};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: u32,
+        a: u32,
+        b: u32,
+    };
+
+    let result = (1..n + 1)
+        .filter(|num| {
+            let sum = num
+                .to_string()
+                .chars()
+                .map(|c| (c as u8 - b'0') as u32)
+                .sum::<u32>();
+            a <= sum && sum <= b
+        })
+        .sum::<u32>();
+    print!("{}", result);
 }
